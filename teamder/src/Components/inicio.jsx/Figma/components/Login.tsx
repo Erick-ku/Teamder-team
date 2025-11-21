75c71b413fc3fc11c337d51f0b861ce59273f8cd
Erick-ku <2402023@utrivieramaya.edu.mx>
2025-11-20 20:54:23 -0500

    agregue y corregi las imagenes y los cables de tailwind-merge

--- teamder/src/Components/inicio.jsx/Figma/components/Login.tsx
+++ teamder/src/Components/inicio.jsx/Figma/components/Login.tsx
@@ -1,8 +1,9 @@
+import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import { Input } from './ui/input';
 import { Button } from './ui/button';
 import { Mail, Lock, Eye, EyeOff, User } from 'lucide-react';
-import teamderLogo from 'figma:asset/5b545e66f6c269c7e1d20d0231f9f7a657db4258.png';
+import teamderLogo from "../../../assets/logo.png";
 
 interface LoginProps {
   onLogin: () => void;
